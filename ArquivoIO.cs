2bf704ac1809b08875257ad7db8543258a0977c4
Kayler <kaylermoura.km@gmail.com>
2024-06-17 15:34:37 -0300

    Refatorando as chamadas de função para o main

--- ArquivoIO.cs
+++ ArquivoIO.cs
@@ -1,6 +1,9 @@
-public class ArquivoIO{
-    public static void LerArquivo(string caminhoArquivoEntrada, string caminhoArquivoSaida){
-        try{
+public class ArquivoIO
+{
+    public static void ProcessarArquivoEntrada(string caminhoArquivoEntrada, List<Course> listaDeCursos)
+    {
+        try
+        {
             //Lê as linhas do arquivo e separa as informações
             var linhas = File.ReadAllLines(caminhoArquivoEntrada);
             var primeiraLinha = linhas[0].Split(' '); //A primeira linha (0) precisa de tratamento especial (n cursos e candidatos)
@@ -9,11 +12,11 @@ public class ArquivoIO{
             int numCandidatos = int.Parse(primeiraLinha[1]);
 
             //Duas listas pra cada classe
-            var listaDeCursos = new List<Course>();
             var listaDeCandidatos = new List<Candidate>();
 
             //Cria cursos lidos e adiciona na lista
-            for (int i = 1; i <= numCursos; i++){ //Começa em 1 para pular a primeira linha (a 0)
+            for (int i = 1; i <= numCursos; i++)
+            { //Começa em 1 para pular a primeira linha (a 0)
                 var dadosCurso = linhas[i].Split(';');
                 int codigoCurso = int.Parse(dadosCurso[0]);
                 string nomeCurso = dadosCurso[1];
@@ -23,7 +26,8 @@ public class ArquivoIO{
             }
 
             //Faz a mesma coisa de cima só que com candidatos
-            for (int i = numCursos + 1; i < linhas.Length; i++){ // numCursos + 1 para pulas as informações dos cursos do arquivo
+            for (int i = numCursos + 1; i < linhas.Length; i++)
+            { // numCursos + 1 para pulas as informações dos cursos do arquivo
                 var dadosCandidato = linhas[i].Split(';'); //Separa as informações em um vetor de dados
                 //Atribui os dados pra variáveis
                 string nomeCandidato = dadosCandidato[0];
@@ -37,64 +41,47 @@ public class ArquivoIO{
                 listaDeCandidatos.Add(new Candidate(nomeCandidato, notaRedacao, notaMatematica, notaLinguagens, codigoPrimeiraOpcao, codigoSegundaOpcao));
             }
 
-            //Imprimindo dados para teste
-            Console.WriteLine("Cursos lidos:");
-            foreach (var curso in listaDeCursos){
-                Console.WriteLine($"{curso.CodigoCurso}; {curso.NomeCurso}; {curso.VagasDisponiveis}");
-            }
-            Console.WriteLine("Candidados lidos:");
-            foreach (var candidate in listaDeCandidatos){
-                Console.WriteLine($"{candidate.Nome}; {candidate.NotaRedacao}; {candidate.NotaMatematica}; {candidate.NotaLinguagens}; {candidate.CodigoPrimeiraOpcao}; {candidate.CodigoSegundaOpcao}");
-            }
-
             /*Percorre toda a lista de candidatos e cursos e adiciona os candidatos na lista de cadidatos daquele curso 
             caso o candidato tenha selecionado em suas opções aquele curso*/
             foreach (var candidato in listaDeCandidatos)
             {
                 foreach (var curso in listaDeCursos)
                 {
-                    if(candidato.CodigoPrimeiraOpcao == curso.CodigoCurso || candidato.CodigoSegundaOpcao == curso.CodigoCurso){
+                    if (candidato.CodigoPrimeiraOpcao == curso.CodigoCurso || candidato.CodigoSegundaOpcao == curso.CodigoCurso)
+                    {
                         curso.Candidatos.Add(candidato);
                     }
                 }
-                
             }
-
-            //Percorre todos os cursos na lista de cursos lidos do arquivo
-            foreach (var curso in listaDeCursos)
-            {
-                curso.OrdenarCandidatos(); //Ordena a classificação de candidatos
-                curso.ClassificarCandidatos(); //Classifica os candidatos nos cursos (Selecionados e Lista de Espera)
-            }
-            
-            var dicionarioDeCursos = listaDeCursos.ToDictionary(c => c.CodigoCurso); /*Cria dicionário a partir da lista. Para cada elemento da lista (chamado de c (lambda) a chave do dicionário será o seu código do curso (c.CodigoCurso))*/
-            /*
-                Chave (TKey): CodigoCurso de cada curso
-                Valor (TValue): O próprio objeto Curso
-            */
-            EscreverArquivoSaida(caminhoArquivoSaida, dicionarioDeCursos);
         }
 
-        catch (Exception ex){
+        catch (Exception ex)
+        {
             Console.WriteLine($"Erro ao tentar ler o arquivo: {ex.Message}");
         }
     }
-     private static void EscreverArquivoSaida(string outputFilePath, Dictionary<int, Course> dicionarioDeCursos){
+
+    //Pega resultado da lista de selecionados e da fila flexível de espera e escreve o resultado no arquivo saida.txt
+    public static void EscreverArquivoSaida(string outputFilePath, Dictionary<int, Course> dicionarioDeCursos)
+    {
         using (StreamWriter writer = new StreamWriter(outputFilePath))
-        {   
+        {
             //Aqui estão sendo escritos os selecionados
-            foreach (var curso in dicionarioDeCursos.Values){
+            foreach (var curso in dicionarioDeCursos.Values)
+            {
                 writer.WriteLine("-------------");
                 double mediaDoCurso = curso.Selecionados.Last().Media;  //Acessa a média do último aprovado na lista de aprovados do curso atual
                 writer.WriteLine($"{curso.NomeCurso} {Math.Round(mediaDoCurso, 2)}");
                 writer.WriteLine("\nSelecionados:");
-                foreach (var candidato in curso.Selecionados){
+                foreach (var candidato in curso.Selecionados)
+                {
                     writer.WriteLine($"{candidato.Nome} {Math.Round(candidato.Media, 2)}");
                 }
 
                 //Aqui estão sendo escritos os da lista de espera
                 writer.WriteLine("\nFila de Espera:");
-                foreach (var candidato in curso.FilaDeEspera){
+                foreach (var candidato in curso.FilaDeEspera)
+                {
                     writer.WriteLine($"{candidato.Nome} {Math.Round(candidato.Media, 2)}");
                 }
             }
--- Program.cs
+++ Program.cs
@@ -1,11 +1,37 @@
 ﻿namespace IO;
 
-class Program{
-    static void Main(string[] args){
-        //Caminhos dos arquivos não tenho certeza como deixar pra submeter...
+class Program
+{
+    static void Main(string[] args)
+    {
+
         string caminhoArquivoEntrada = "./test.txt";
         string caminhoArquivoSaida = "./saida.txt";
-        
-        ArquivoIO.LerArquivo(caminhoArquivoEntrada, caminhoArquivoSaida);
+
+        var listaDeCursos = new List<Course>(); //Lista pra armazenar os cursos com seus candidatos e resultados
+
+        ArquivoIO.ProcessarArquivoEntrada(caminhoArquivoEntrada, listaDeCursos);
+
+        //Percorre todos os cursos na lista de cursos lidos do arquivo para ordenar e classificar os seus candidatos
+        foreach (var curso in listaDeCursos)
+        {
+            curso.OrdenarCandidatos(); //Ordena a classificação de candidatos
+            curso.ClassificarCandidatos(); //Classifica os candidatos nos cursos (Selecionados e Lista de Espera)
+        }
+
+        var dicionarioDeCursos = listaDeCursos.ToDictionary(c => c.CodigoCurso); /*Cria dicionário a partir da lista. Para cada elemento da lista (chamado de c (lambda) a chave do dicionário será o seu código do curso (c.CodigoCurso))*/
+        /*
+            Chave (TKey): CodigoCurso de cada curso
+            Valor (TValue): O próprio objeto Curso
+        */
+
+        //Recebe o dicionária criado a partir da lista anterior e escreve o arquivo saida.txt
+        ArquivoIO.EscreverArquivoSaida(caminhoArquivoSaida, dicionarioDeCursos);
+
+
+        //Mensagem para ser exibida no console confirmando o sucesso da operação
+        System.Console.WriteLine("\a\nProcessamento realizado com sucesso!");
+        System.Console.WriteLine("Arquivo saida.txt atualizado");
+
     }
 }
